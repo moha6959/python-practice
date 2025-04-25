f6b0670d22da2d7ea42a9972c32ee3cd5ccf3708
moha6959 <mohamedekaden576@gmail.com>
2025-04-25 21:10:42 +0300

    added comments on my code

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-number=int(input("Enter a number? "))
-if number % 2==0:
+number=int(input("Enter a number? "))#asks a user to enter a number of his choice
+if number % 2==0:  #checks if the number is divisible by 2
     print("Even ")
 else:
     print("odd")
