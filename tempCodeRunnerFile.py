e6df6d120ae94ad95a5be37b765e46a4854300b8
moha6959 <mohamedekaden576@gmail.com>
2025-04-25 22:51:15 +0300

    Generates a reversed string without use of built in methods

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,5 @@
-num=int(input("Enter a number? "))
-factorial=1
-if num<0:
-    print("there is no factorial for negative numbers")
-elif num==0:
-    print("factorial of is 1")
-else:
-    for i in range(1,num+1):
-        factorial=factorial*i
-print("The factorial is: ", factorial)
+message="How to reverse a string"
+new_message=""
+for i in message:
+    new_message=i+new_message
+print(new_message)
