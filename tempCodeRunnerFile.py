838d9b6077362b0072189da51950126f69154cf4
moha6959 <mohamedekaden576@gmail.com>
2025-04-26 15:09:37 +0300

    calculates the sum of digit of numbers

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
-def factorial(n):
-    if n==0:
-        return 1
-    elif n==1:
-        return 1
-    else:
-        return n*factorial(n-1)
-print(factorial(8))
+n=int(input("Enter a number? "))
+#initialize sum to 0
+sum=0
+while n!=0:
+    digit=n%10 #gets the last digit of n
+    sum+=digit
+    n=n//10  #removes the last digit of n
+print("The sum of the digits is: ",sum)
